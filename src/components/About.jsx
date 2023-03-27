5b9054c43aa87318c9d89cf0ee4f2d120131f053
Blagoja0123 <dzorlevblagoja@yahoo.com>
2023-03-27 20:10:46 +0200

    lightblue-teal-gradient

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import {motion} from 'framer-motion';
+import { fadeIn } from '../utils/motion';
 
 
 const Card = ({index, title, icon}) =>{
-    <Tilt className = 'xs:w-[250px] w-full'>
-        <motion.div>
+    <Tilt className='xs:w-[250px] w-full'>
+        <motion.div
+        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+        className='w-full '
+        >
             
         </motion.div>
     </Tilt>
