00b2139c7ab860ac9280e0586aeddc92d92ad159
Blagoja0123 <dzorlevblagoja@yahoo.com>
2023-04-01 00:22:34 +0200

    svg-background-fix-attemp-02

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       <div className='relative z-0 bg-primary'>
-          <div className='bg-hero-pattern bg-cover bg-center'>
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
             <Nav/>
             <Hero />
           </div>
